32eb160b669b106affaee4271040e21ae4bcb653
haider000 <mohd.b.haider000786@gmail.com>
2024-04-16 23:48:39 +0530

    🚑 added health check route

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from "dotenv"
-import postRoutes from "./routes/posts.js"
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import postRoutes from "./routes/posts.js";
 import userRouter from "./routes/user.js";
 
 dotenv.config();
 const app = express();
+const router = express.Router();
 
-app.use(bodyParser.json({limit:"30mb", extended:true }));
-app.use(bodyParser.urlencoded({limit:"30mb", extended:true}))
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes);
+app.use("/posts", postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL  = process.env.CONNECTION_URL;
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    msg: "sucsess",
+    data: "Hitted backend, up and running!",
+  });
+});
+
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology:true})
-    .then( () => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
-    .catch((error) => {console.log("Error:" + error.message )});
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port : ${PORT}`))
+  )
+  .catch((error) => {
+    console.log("Error:" + error.message);
+  });
 
-mongoose.set('useFindAndModify', false);
+mongoose.set("useFindAndModify", false);
