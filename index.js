290cee273b9e03e13e747d3ab767dc74c61f266b
haider000 <mohd.b.haider000786@gmail.com>
2024-04-16 19:41:22 +0530

    changed db url

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL  = "mongodb+srv://haider:haider@000@notes.eg4yq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const CONNECTION_URL  = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology:true})
